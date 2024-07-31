9a36cdb0ac3d23f9f765d2f9bd085a9e5b8484cd
kimquyen <kimquyen.tttgg>
2024-07-31 19:16:52 +0700

    thaydoi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import Header from "./components/Header/Header";
-import Intro from "./components/Intro/Intro"
-
+// import Header from "./components/Header/Header";
+// import Intro from "./components/Intro/Intro"
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className='App'>
-      <Header/>
-      <Intro/>
+      <Home/>
     </div>
   );
 }
